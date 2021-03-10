b78b6dbe4cb03b4288e32fe3443a2ff79e549bf2
Thiago Marchini <thiago@msn.com>
2021-03-10 10:42:09 -0300

    aplica as mudanças no projeto pixels-art

--- projects/pixels-art/script.js
+++ projects/pixels-art/script.js
@@ -46,42 +46,51 @@ function removeAllChildNodes(parent) {
   }
 }
 
+function resizeMinMax(parameter) {
+  if (parameter < 5) {
+    return 5;
+  }
+  if (parameter > 50) {
+    return 50;
+  }
+  return parameter;
+}
+function clearBoard() {
+  const pixelList = document.querySelectorAll('.pixel');
+  for (let index = 0; index < pixelList.length; index += 1) {
+    pixelList[index].style.backgroundColor = 'white';
+  }
+}
+function paintPixel(pixel) {
+  const selected = document.getElementsByClassName('selected');
+  const modified = pixel;
+  modified.style.backgroundColor = selected[0].style.backgroundColor;
+}
+function selectColor(selected) {
+  document.querySelectorAll('.selected')[0].className = 'color';
+  const modified = selected;
+  modified.className = 'color selected';
+}
+function recreateBoard(size) {
+  const newSize = resizeMinMax(size);
+  const parent = document.getElementById('pixel-board');
+  removeAllChildNodes(parent);
+  createGrid(Number(newSize));
+}
 window.onload = function () {
-  // cria a paleta de cores
   createPalette();
-
-  // cria o grid de N por N
   createGrid(5);
-
   document.addEventListener('click', function (event) {
     if (event.target.classList.contains('color') && !event.target.classList.contains('selected')) {
-      document.querySelectorAll('.selected')[0].className = 'color';
-      event.target.className = 'color selected';
+      selectColor(event.target);
     }
-
-    if (event.target.classList.contains('pixel')) {
-      const selected = document.getElementsByClassName('selected');
-      event.target.style.backgroundColor = selected[0].style.backgroundColor;
-    }
-
-    if (event.target.id === 'clear-board') {
-      const pixelList = document.querySelectorAll('.pixel');
-      for (let pixel of pixelList) {
-        pixel.style.backgroundColor = 'white';
-      }
-    }
-
+    if (event.target.classList.contains('pixel')) { paintPixel(event.target); }
+    if (event.target.id === 'clear-board') { clearBoard(); }
     if (event.target.id === 'generate-board') {
       const size = document.getElementById('board-size');
       if (size.value && size.value > 0) {
-        if (size.value < 5) { size.value = 5; }
-        if (size.value > 50) { size.value = 50; }
-        const parent = document.getElementById('pixel-board');
-        removeAllChildNodes(parent);
-        createGrid(Number(size.value));
-      } else {
-        alert('Board inválido!');
-      }
+        recreateBoard(size.value);
+      } else { alert('Board inválido!'); }
     }
   }, false);
 };
