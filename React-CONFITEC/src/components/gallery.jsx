0d64628ea758730899df4dc793737995485cd0d6
Thiago Marchini <thiago@msn.com>
2021-08-08 06:22:01 -0300

    Adicionado React-CONFITEC

---
+++ React-CONFITEC/src/components/gallery.jsx
@@ -0,0 +1,154 @@
+export const Gallery = (props) => {
+  return (
+    <div id='portfolio' className='text-center'>
+      <div className='container'>
+        <div className='section-title'>
+          <h2>Galeria</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed
+            dapibus leonec.
+          </p>
+        </div>
+        <div className='row'>
+          <div className='portfolio-items'>
+            <div className='col-sm-6 col-md-4 col-lg-4'>
+              <div className='portfolio-item'>
+                <div className='hover-bg'>
+                  {' '}
+                    <div className='hover-text'>
+                      <h4>Lorem Ipsum</h4>
+                    </div>
+                    <img
+                      src='img/portfolio/01-small.jpg'
+                      className='img-responsive'
+                      alt='Project Title'
+                    />{' '}
+                </div>
+              </div>
+            </div>
+            <div className='col-sm-6 col-md-4 col-lg-4'>
+              <div className='portfolio-item'>
+                <div className='hover-bg'>
+                  {' '}
+                    <div className='hover-text'>
+                      <h4>Adipiscing Elit</h4>
+                    </div>
+                    <img
+                      src='img/portfolio/02-small.jpg'
+                      className='img-responsive'
+                      alt='Project Title'
+                    />{' '}
+                </div>
+              </div>
+            </div>
+            <div className='col-sm-6 col-md-4 col-lg-4'>
+              <div className='portfolio-item'>
+                <div className='hover-bg'>
+                  {' '}
+                    <div className='hover-text'>
+                      <h4>Lorem Ipsum</h4>
+                    </div>
+                    <img
+                      src='img/portfolio/03-small.jpg'
+                      className='img-responsive'
+                      alt='Project Title'
+                    />{' '}
+                </div>
+              </div>
+            </div>
+            <div className='col-sm-6 col-md-4 col-lg-4'>
+              <div className='portfolio-item'>
+                <div className='hover-bg'>
+                  {' '}
+                    <div className='hover-text'>
+                      <h4>Lorem Ipsum</h4>
+                    </div>
+                    <img
+                      src='img/portfolio/04-small.jpg'
+                      className='img-responsive'
+                      alt='Project Title'
+                    />{' '}
+                </div>
+              </div>
+            </div>
+            <div className='col-sm-6 col-md-4 col-lg-4'>
+              <div className='portfolio-item'>
+                <div className='hover-bg'>
+                  {' '}
+                    <div className='hover-text'>
+                      <h4>Adipiscing Elit</h4>
+                    </div>
+                    <img
+                      src='img/portfolio/05-small.jpg'
+                      className='img-responsive'
+                      alt='Project Title'
+                    />{' '}
+                </div>
+              </div>
+            </div>
+            <div className='col-sm-6 col-md-4 col-lg-4'>
+              <div className='portfolio-item'>
+                <div className='hover-bg'>
+                  {' '}
+                    <div className='hover-text'>
+                      <h4>Dolor Sit</h4>
+                    </div>
+                    <img
+                      src='img/portfolio/06-small.jpg'
+                      className='img-responsive'
+                      alt='Project Title'
+                    />{' '}
+                </div>
+              </div>
+            </div>
+            <div className='col-sm-6 col-md-4 col-lg-4'>
+              <div className='portfolio-item'>
+                <div className='hover-bg'>
+                  {' '}
+                    <div className='hover-text'>
+                      <h4>Dolor Sit</h4>
+                    </div>
+                    <img
+                      src='img/portfolio/07-small.jpg'
+                      className='img-responsive'
+                      alt='Project Title'
+                    />{' '}
+                </div>
+              </div>
+            </div>
+            <div className='col-sm-6 col-md-4 col-lg-4'>
+              <div className='portfolio-item'>
+                <div className='hover-bg'>
+                  {' '}
+                    <div className='hover-text'>
+                      <h4>Lorem Ipsum</h4>
+                    </div>
+                    <img
+                      src='img/portfolio/08-small.jpg'
+                      className='img-responsive'
+                      alt='Project Title'
+                    />{' '}
+                </div>
+              </div>
+            </div>
+            <div className='col-sm-6 col-md-4 col-lg-4'>
+              <div className='portfolio-item'>
+                <div className='hover-bg'>
+                  {' '}
+                    <div className='hover-text'>
+                      <h4>Adipiscing Elit</h4>
+                    </div>
+                    <img
+                      src='img/portfolio/09-small.jpg'
+                      className='img-responsive'
+                      alt='Project Title'
+                    />{' '}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
